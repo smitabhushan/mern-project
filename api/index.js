7700b2cfb2c26cdab9ae58d0d734819a296fe9ca
Smita Priya <smitabhushan58@gmail.com>
2024-01-21 21:11:26 +0530

    connected to database

--- api/index.js
+++ api/index.js
@@ -1,4 +1,14 @@
 import express from'express'
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+dotenv.config();// we are using enviromental variable 
+mongoose.connect(process.env.MONGO).then(()=>{
+console.log("connected to mongodb");
+
+}).catch((err)=>{
+   console.log(err);
+})
+
 const server=express();
 
 server.listen(3000,()=>{
