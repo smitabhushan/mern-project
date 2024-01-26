c2f8941428084cfa06351d41173d630aef67000c
Smita Priya <smitabhushan58@gmail.com>
2024-01-26 17:19:56 +0530

    create sign in api route

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,6 +1,8 @@
 import User from "../models/userModel.js";
 import bcryptjs from 'bcryptjs';// password hash krne ke liye bcryptjs install krnge
 import { errorHandler } from "../utils/error.js";
+import jwt from 'jsonwebtoken';
+
 export const signup=async (req,res,next)=>{
   console.log(req.body);
   const {username,email,password}=req.body;
@@ -16,4 +18,29 @@ export const signup=async (req,res,next)=>{
 
   }
   
+};
+
+export const signin=async(req,res,next)=>{
+  const{email,password}=req.body;
+  try{
+    const validUser=await User.findOne({email});
+    if(!validUser){
+      return next(errorHandler(404,'User Not Found'));
+    }
+    const validPassword=bcryptjs.compareSync(password,validUser.password);
+    if(!validPassword){
+      return next(errorHandler(401,'Wrong Credential!'));
+    }
+  // using JWT to create web token for authentication process
+  const token= jwt.sign({id:validUser._id }, process.env.JWT_SECRET) // created the token
+   const {password:pass , ...rest }=validUser._doc; // destructing the password as pass and all other data in rest
+  // now we want to save this token as our Cookie.
+   res.cookie('User_token',token ,{httpOnly:true})
+   .status(200)
+   .json({rest}) // now we have a cookie inside our browser
+
+  }
+  catch(error){
+    next(error);
+  }
 }
