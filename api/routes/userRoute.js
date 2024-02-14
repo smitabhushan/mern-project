bcca67904f9a877fdd2f3d112d35d3395224c58c
Smita Priya <smitabhushan58@gmail.com>
2024-02-14 21:26:25 +0530

    create get user listings api route

--- api/routes/userRoute.js
+++ api/routes/userRoute.js
@@ -1,6 +1,6 @@
 // hum api routes alag ALAG file mein banaynge , bas api route ko define kr denge index.js ke andar
 import express from 'express';
-import { deleteUser, test, updateUser } from '../controllers/user.controller.js';
+import { deleteUser, test, updateUser ,getUserListings} from '../controllers/user.controller.js';
 import { verifyToken } from '../utils/verifyUser.js';
 const router=express.Router();
 
@@ -12,5 +12,6 @@ router.get('/test' , test)
 //api route for update
 router.post('/update/:id' ,verifyToken, updateUser);
 router.delete('/delete/:id' , verifyToken,deleteUser);
+router.get('/listings/:id' , verifyToken, getUserListings);
 export default router;
 
