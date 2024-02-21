6b535982a27890a241f95671bc3a7eea3c878a00
Smita Priya <smitabhushan58@gmail.com>
2024-02-21 21:12:51 +0530

    new commit

--- client/vite.config.js
+++ client/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   server:{
     proxy:{
       '/api' : {
-        target:'https://mern-project-2-fxm1.onrender.com' ,
+        target:'http://localhost:3000' ,
         secure:false,
       },
     },
